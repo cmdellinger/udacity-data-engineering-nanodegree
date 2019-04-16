1f13af95073ca050bf74ecc073bce1b91e970a6d
Charles M Dellinger <charles.dellinger@gmail.com>
2019-04-16 16:39:23 -0400

    project 03 - data modeling - etl and sql_queries implemented EXCEPT the FIND

--- 02 - Data Modeling/03 - project - data modeling with postgres/project/sql_queries.py
+++ 02 - Data Modeling/03 - project - data modeling with postgres/project/sql_queries.py
@@ -1,93 +1,122 @@
 # DROP TABLES
 
-songplay_table_drop = "DROP table songplays"
-user_table_drop = "DROP table users"
-song_table_drop = "DROP table songs"
-artist_table_drop = "DROP table artists"
-time_table_drop = "DROP table time"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
+#Fact table
 songplay_table_create = ("""
 CREATE TABLE IF NOT EXISTS songplays
-(songplay_id int PRIMARY KEY,
- start_time REFERENCES time(start_time),
- user_id REFERENCES users(user_id),
- level REFERENCES users(level),
- song_id REFERENCES songs(song_id),
- artist_id REFERENCES artists(artist_id),
+(songplay_id SERIAL PRIMARY KEY,
+ start_time TIMESTAMP,
+ user_id INTEGER,
+ level VARCHAR,
+ song_id VARCHAR,
+ artist_id VARCHAR,
  session_id INTEGER,
  location TEXT,
  user_agent TEXT
 );
 """)
 
+#Dimension tables
+
+# gender is 'M' or 'F'
+# levels is 'free' or 'paid'
 user_table_create = ("""
 CREATE TABLE IF NOT EXISTS users
 (user_id INTEGER PRIMARY KEY,
  first_name VARCHAR,
  last_name VARCHAR,
- gender VARCHAR,
- level VARCHAR
+ gender CHAR(1),
+ level CHAR(4)
 );
 """)
 
 song_table_create = ("""
 CREATE TABLE IF NOT EXISTS songs
 (song_id VARCHAR PRIMARY KEY,
- title TEXT,
- artist_id REFERENCES artists(artist_id),
+ title VARCHAR,
+ artist_id VARCHAR,
  year INTEGER,
- duration NUMERIC
+ duration DECIMAL
 );
 """)
 
 artist_table_create = ("""
 CREATE TABLE IF NOT EXISTS artists
-(artist_id INTEGER PRIMARY KEY,
+(artist_id VARCHAR PRIMARY KEY,
  name VARCHAR,
  location TEXT,
- lattitude NUMBERIC,
- longitude NUMBERIC
+ latitude NUMERIC,
+ longitude NUMERIC
 );
 """)
 
 time_table_create = ("""
 CREATE TABLE IF NOT EXISTS time
-(start_time NUMBERIC PRIMARY KEY,
+(start_time TIMESTAMP,
  hour INTEGER,
  day INTEGER,
  week INTEGER,
  month INTEGER,
  year INTEGER,
- weekday INTEGER
+ weekday TEXT
 );
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+INSERT INTO songplays
+(start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
 user_table_insert = ("""
+INSERT INTO users
+(user_id, first_name,last_name, gender, level)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (user_id)
+DO UPDATE SET level=EXCLUDED.level
 """)
 
 song_table_insert = ("""
+INSERT INTO songs
+(song_id, title, artist_id, year, duration)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (song_id)
+DO NOTHING
 """)
 
 artist_table_insert = ("""
+INSERT INTO artists
+(artist_id, name, location, latitude, longitude)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (artist_id)
+DO NOTHING
 """)
 
-
 time_table_insert = ("""
+INSERT INTO time
+(start_time, hour, day, week, month, year, weekday)
+VALUES (%s, %s, %s, %s, %s, %s, %s)
 """)
 
 # FIND SONGS
 
 song_select = ("""
+SELECT songs.song_id, artists.artist_id
+FROM songs
+JOIN artists ON songs.artist_id = artists.artist_id
+WHERE songs.title = %s OR artists.name = %s OR songs.duration = %s;
 """)
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [artist_table_create, time_table_create, user_table_create, song_table_create, songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
