4a241856f57ee8e135eec8eba60cb86a273a0a46
Charles M Dellinger <charles.dellinger@gmail.com>
2019-06-21 09:43:30 -0400

    05 - data lakes with spark - 05 - project - data lakes with AWS
    completed project to create a data lake by processing json files using Spark on Amazon EMR and storing them as parquet files on S3.

---
+++ 05 - data lakes with spark/05 - project - data lakes/project/README.md
@@ -0,0 +1,109 @@
+# Project: Data Lakes with AWS
+
+The purpose of this project is to create an ETL pipline that transforms data from two types of JSON log files stored in directory trees into Spark DataFrames instead of tables following a database schema. The source data is song and user activity logs from a simulated music streaming app, Sparkify. The firm intends to move to a data lake, in this case Amazon Web Services (AWS). The data is stored in an S3 bucket, will be loaded into Spark DataFrames, and tranformed into the schema DataFrame tables. After loading the data into tables, the data will be stored as parquet files for faster, preprocessed loading.
+
+<img src="/images/data_flow.png" alt="ETL_pipeline" style="width:50%">
+
+---
+
+## Data Files
+
+The two sets of logs are located in S3 buckets.
+
+_S3 location_:
+```python
+s3://udacity-dend/song_data
+```
+_S3 location_:
+```python
+s3://udacity-dend/log_data
+```
+
+### _Song Dataset_
+
+The song data is stored in directory trees based on the first three letters of each song's track ID. 
+
+_Example file structure_:
+```javascript
+song_data/A/A/C/TRAACCG128F92E8A55.json
+```
+
+### _Log Dataset_
+
+The log data contains simlated event data for the music streaming app.
+
+_Example file structure_:
+```javascript
+log_data/2018/11/2018-11-01-events.json
+log_data/2018/11/2018-11-02-events.json
+```
+
+---
+
+## Data Schema
+
+To save on the costs associated with reprocessing the data or keeping a cluster running, the DataFrame tables will be stored as parquet files for quick loading back into Spark DataFrames.
+
+The ouput table schema is created using a **_star schema_** to mirror to their previous database. Although the schema is slightly denormalized with some duplicate data fields between tables, it will optimize the song play queries that Sparkify would like to run, especially since `JOIN`s on large data sets start becoming very resource intensive.
+
+Previous database schema that Spark DataFrames will mirror:
+<img src="/images/Sparkify_SQL_Schema.png" alt="Sparkify SQL Schema" style="width:100%">
+
+---
+
+## Files
+<table>
+  <tr>
+    <th>File</th>
+    <th>Description</th> 
+  </tr>
+  <tr>
+    <td>data/</td>
+    <td>Folder that contains subset of data for local testing.</td> 
+  </tr>
+  <tr>
+    <td>images/</td>
+    <td>Folder that contains any images used in more robust files, like README or ipynb.</td> 
+  </tr>
+  <tr>
+    <td>etl.ipynb</td>
+    <td>Jupyter notebook to be used when linked to an EMR cluster.</td> 
+  </tr>
+  <tr>
+    <td>testing_ground.ipynb</td>
+    <td>Jupyter notebook that sets up a local instance and works on sample dataset (requires working spark install).</td> 
+  </tr>
+  <tr>
+    <td>dl.cfg</td>
+    <td>Configuration file that contains IAM credentials.</td> 
+  </tr>
+  <tr>
+    <td>etl.py</td>
+    <td>Python script that calls SQL queries to perform ETL operations.</td> 
+  </tr>
+  <tr>
+    <td>README.md</td>
+    <td>This file. Contains README for contents and usage.</td> 
+  </tr>
+</table>
+
+---
+
+## Usage
+<b>Requirements</b>:
+- Amazon EMR cluster
+- IAM role with:
+    + EMR cluster standard permissions
+    + S3 read/write permissions
+
+<b>Usage</b>:
+- Method 1: EC2 Instance
+    - Add AWS keys for IAM role information into `dl.cfg`.
+    - Fill in input and output locations (directory or S3).
+    - Create an EC2 instance linked to the EMR cluster.
+    - Run `python etl.py` in EC2 instance to create parquet files.
+- Method 2: EMR Jupter Notebook
+    - Link `etl.ipynb` in AWS under `EMR > Notebooks`
+    - Add AWS keys for IAM role information into environmental variables.
+    - Fill in input and output locations (directory or S3).
+    - Run cells to process data into parquet files.
--- 05 - data lakes with spark/05 - project - data lakes/project/etl.py
+++ 05 - data lakes with spark/05 - project - data lakes/project/etl.py
@@ -1,92 +1,190 @@
+""" ETL pipline
+    
+Ingests JSON files in 'input_data/log_data' and 'input_data/song_data' trees
+into Spark DataFrames. The DataFrames are transformed into other DataFrames
+that contain the columns corresponding to the schema in `README.md`. The
+processed data is then saved to parquet file trees in 'output_data/'
+"""
 import configparser
 from datetime import datetime
 import os
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf, col
+from pyspark.sql.functions import udf, col, monotonically_increasing_id
 from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
+from pyspark.sql.functions import to_date, to_timestamp
 
-
+# load AWS credentials from config file into enviromental variables
 config = configparser.ConfigParser()
 config.read('dl.cfg')
 
-os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
-os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
+os.environ['AWS_ACCESS_KEY_ID']=config.get('AWS','AWS_ACCESS_KEY_ID')
+os.environ['AWS_SECRET_ACCESS_KEY']=config.get('AWS','AWS_SECRET_ACCESS_KEY')
 
 
+def ts_msg(message):
+    """ prints string with a timestamp """
+    timestamp = "{} ".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    print(timestamp + message)
+    
 def create_spark_session():
+    """ creates Spark session with AWS hadoop package """
     spark = SparkSession \
         .builder \
-        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
+        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.2.0") \
         .getOrCreate()
     return spark
 
-
 def process_song_data(spark, input_data, output_data):
+    """ Loads song data into songs and artists tables and saves them as parquet.
+        
+    The function ingests data into a Spark DataFrame from the song JSON files
+    located at 'input_data/song_data/'. The applicable data columns are parsed
+    and transformed based on the `songs` and `artists` table schema. The tables
+    are then save as parquet file/directory tree in the `output_data` directory.
+    
+    Args:
+        spark (SparkSession): SparkSession object of Spark cluster/instance
+        intput_data (str): root filepath containing `song_data` directory
+        output_data (str): root filepath of directory to save parquet file trees
+    Returns:
+        `None`: actions performed, but no return value
+    """
+    
     # get filepath to song data file
-    song_data = 
+    song_data = os.path.join(input_data, 'song_data/*/*/*/*.json')
     
     # read song data file
-    df = 
-
-    # extract columns to create songs table
-    songs_table = 
+    df = spark.read.json(song_data)
     
+    # extract columns to create songs table
+    ts_msg("selecting songs_table")
+    songs_table = df.select("song_id", \
+                            "title", \
+                            "artist_id", \
+                            "year", \
+                            "duration") \
+                    .dropDuplicates()
+            
     # write songs table to parquet files partitioned by year and artist
-    songs_table
+    ts_msg("writing songs_table")
+    songs_table.write.parquet(os.path.join(output_data, "songs_table"), \
+                              mode='overwrite', \
+                              partitionBy=["year","artist_id"])
 
     # extract columns to create artists table
-    artists_table = 
+    ts_msg("selecting artists_table")
+    artists_table = df.select('artist_id', \
+                              col('artist_name').alias('name'), \
+                              col('artist_location').alias('location'), \
+                              col('artist_latitude').alias('latitude'), \
+                              col('artist_longitude').alias('longitude')) \
+                      .dropDuplicates()
     
     # write artists table to parquet files
-    artists_table
-
-
+    ts_msg("writing artists_table")
+    artists_table.write.parquet(os.path.join(output_data, "artists_table"), \
+                                mode='overwrite')
+    
 def process_log_data(spark, input_data, output_data):
+    """ Loads log data to users, time, & songplays tables and saves as parquet.
+        
+    The function ingests data into a Spark DataFrame from the log JSON files
+    located at 'input_data/log_data/'. The applicable data columns are parsed
+    and transformed based on the `users`, `time`, and `songplays` table schema.
+    The tables are then save as parquet file/directory tree in the `output_data`
+    directory.
+    
+    Args:
+        spark (SparkSession): SparkSession object of Spark cluster/instance
+        intput_data (str): root filepath containing `song_data` directory
+        output_data (str): root filepath of directory to save parquet file trees
+    Returns:
+        `None`: actions performed, but no return value
+    """
     # get filepath to log data file
-    log_data =
-
+    log_data = os.path.join(input_data, 'log_data/*.json')
+    
     # read log data file
-    df = 
+    df = spark.read.json(log_data)
     
     # filter by actions for song plays
-    df = 
-
-    # extract columns for users table    
-    artists_table = 
+    df = df.filter(df.page == 'NextSong')
     
+    # extract columns for users table
+    ts_msg("selecting users_table")
+    users_table = df.select(col('userId').alias('user_id'), \
+                            col('firstName').alias('first_name'), \
+                            col('lastName').alias('last_name'), \
+                            'gender', \
+                            'level') \
+                    .dropDuplicates()
+            
     # write users table to parquet files
-    artists_table
+    ts_msg("writing users_table")
+    users_table.write.parquet(os.path.join(output_data, "users_table"), \
+                               mode='overwrite')
 
     # create timestamp column from original timestamp column
-    get_timestamp = udf()
-    df = 
-    
+    df = df.withColumn('timestamp', to_timestamp(df.ts/1000))
+
     # create datetime column from original timestamp column
-    get_datetime = udf()
-    df = 
+    df = df.withColumn('datetime', to_date(df.timestamp))
     
     # extract columns to create time table
-    time_table = 
-    
+    # start_time, hour, day, week, month, year, weekday
+    ts_msg("selecting time_table")
+    time_table = df.select(col('timestamp').alias('start_time'), \
+                           hour(col('datetime')).alias('hour'), \
+                           dayofmonth(col('datetime')).alias('day'), \
+                           weekofyear(col('datetime')).alias('week'), \
+                           month(col('datetime')).alias('month'), \
+                           year(col('datetime')).alias('year'), \
+                           date_format(col('datetime'), 'E').alias('weekday')) \
+                    .dropDuplicates()
+            
     # write time table to parquet files partitioned by year and month
-    time_table
+    ts_msg("writing time_table")
+    time_table.write.parquet(os.path.join(output_data, "time_table"), \
+                             mode='overwrite', \
+                             partitionBy=["year","month"])
 
     # read in song data to use for songplays table
-    song_df = 
+    ts_msg("loading songs_table")
+    song_df = spark.read.parquet(os.path.join(output_data, 'songs_table'))
 
-    # extract columns from joined song and log datasets to create songplays table 
-    songplays_table = 
+    # extract columns from joined song and log datasets to create songplays table
+    ts_msg("selecting songplays_table")
+    songplays_table = df.join(song_df, df.song == song_df.title) \
+                        .select(col('timestamp').alias('start_time'), \
+                                col('userId').alias('user_id'), \
+                                'level', \
+                                'song_id', \
+                                'artist_id', \
+                                col('sessionId').alias('session_id'), \
+                                'location', \
+                                col('userAgent').alias('user_agent'), \
+                                year('timestamp').alias('year'), \
+                                month('timestamp').alias('month')) \
+                        .dropDuplicates() \
+                        .withColumn('songplay_id', monotonically_increasing_id())
 
     # write songplays table to parquet files partitioned by year and month
-    songplays_table
-
+    ts_msg("writing songplays_table")
+    songplays_table.write.parquet(os.path.join(output_data, "songplays_table"), \
+                                  mode='overwrite', \
+                                  partitionBy=["year","month"])
 
 def main():
     spark = create_spark_session()
-    input_data = "s3a://udacity-dend/"
-    output_data = ""
     
-    process_song_data(spark, input_data, output_data)    
+    # input and output directories or S3 paths
+    # note: don't use trailing '/' 
+    input_data = "s3a://sampleS3bucket/input_data"
+    output_data = "s3a:/sampleS3bucket/output_data"
+    
+    print("===== processing song_data =====")
+    process_song_data(spark, input_data, output_data)
+    print("===== processing log_data =====")
     process_log_data(spark, input_data, output_data)
 
 
